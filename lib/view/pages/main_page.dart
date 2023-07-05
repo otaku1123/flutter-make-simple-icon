f4977e6792ab810cbfe78dbd7b93777a82715a21
Taku Ogawa <ogawtaku@gmail.com>
2023-07-05 15:24:49 +0900

    設定画面を追加

--- lib/view/pages/main_page.dart
+++ lib/view/pages/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:make_simple_image_app/view/pages/settings_page.dart';
 
 class MainPage extends ConsumerWidget {
   const MainPage({super.key});
@@ -8,7 +9,30 @@ class MainPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Demo'),
+        title: const Text('Main Page'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return const SettingsPage();
+                      },
+                    ),
+                  );
+                },
+                child: const Text('Settings'),
+              ),
+            ),
+          ],
+        ),
       ),
       body: const Center(
         child: Text('Hello World'),
