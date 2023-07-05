ce6a79dddfe3e5e8764427820bb295fd6579bfcb
Taku Ogawa <ogawtaku@gmail.com>
2023-07-05 23:43:54 +0900

    Stateを更新

--- lib/application/state/main_page_state.dart
+++ lib/application/state/main_page_state.dart
@@ -1,38 +1,51 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainPageState {
   String text;
-  // String font;
-  // Color fontColor;
-  // int fontSize;
-  // Color backgroundColor;
-  // Alignment textPosition;
+  String font;
+  int fontSize;
+  FontWeight fontWeight;
+  Color fontColor;
+  Color backgroundColor;
+  int letterSpacing;
+  int wordSpacing;
+  // memo: 下線などは定義していない
+
+  Alignment textAlign;
 
   MainPageState({
     this.text = 'おがわ',
-    // this.font = 'Roboto',
-    // this.fontColor = Colors.black,
-    // this.fontSize = 50,
-    // this.backgroundColor = Colors.greenAccent,
-    // this.textPosition = Alignment.center,
+    this.font = 'Roboto',
+    this.fontSize = 50,
+    this.fontWeight = FontWeight.normal,
+    this.fontColor = Colors.black,
+    this.backgroundColor = Colors.greenAccent,
+    this.letterSpacing = 0,
+    this.wordSpacing = 0,
+    this.textAlign = Alignment.center,
   });
 
   MainPageState copyWith({
     String? text,
-    // String? font,
-    // Color? fontColor,
-    // int? fontSize,
-    // Color? backgroundColor,
-    // Alignment? textPosition,
+    String? font,
+    int? fontSize,
+    FontWeight? fontWeight,
+    Color? fontColor,
+    Color? backgroundColor,
+    int? letterSpacing,
+    int? wordSpacing,
+    Alignment? textAlign,
   }) {
     return MainPageState(
       text: text ?? this.text,
-      // font: font ?? this.font,
-      // fontColor: fontColor ?? this.fontColor,
-      // fontSize: fontSize ?? this.fontSize,
-      // backgroundColor: backgroundColor ?? this.backgroundColor,
-      // textPosition: textPosition ?? this.textPosition,
+      font: font ?? this.font,
+      fontSize: fontSize ?? this.fontSize,
+      fontWeight: fontWeight ?? this.fontWeight,
+      fontColor: fontColor ?? this.fontColor,
+      backgroundColor: backgroundColor ?? this.backgroundColor,
+      letterSpacing: letterSpacing ?? this.letterSpacing,
+      wordSpacing: wordSpacing ?? this.wordSpacing,
+      textAlign: textAlign ?? this.textAlign,
     );
   }
 }
